775899ae516a1a73c0da929780178e6a173782a0
Sandipan <sandipanseth2004@gmail.com>
2025-04-03 20:09:17 +0530

    Some

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About/About';
+// import { ThreeDCardDemo } from './components/Card';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
@@ -19,6 +20,7 @@ function App() {
       <Services />
       <MyWork />
       <Contact />
+      {/* <ThreeDCardDemo /> */}
       <Footer />
     </>
   );

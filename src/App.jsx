8eb4254ae9cf901bc71cc2039bb225645eb50959
Sandipan Seth <sandipanseth2004@gmail.com>
2026-01-08 20:13:24 +0530

    whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import MyWork from './components/MyWork/MyWork';
 import Navbar from './components/Navbar/Navbar';
 import Services from './components/Services/Services';
 import Top from './components/Top';
-import Whatsapp from './components/whatsapp';
+import Whatsapp from './components/Whatsapp';
 import './index.css'
 
 function App() {
